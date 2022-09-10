45b96c1da3a1fbc5b4f6f4f45a38b703e8fd8704
Alexandre Marcos Luz <amsljr@hotmail.com>
2022-09-10 18:52:41 -0300

    API Test 2

---
+++ backend/src/main/java/com/projetosdev/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.projetosdev.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetosdev.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
