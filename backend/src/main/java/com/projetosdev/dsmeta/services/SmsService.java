0a4f80fe299f95dd29a0869a32a34b2a5f6b1d9a
Alexandre Marcos Luz <amsljr@hotmail.com>
2022-09-16 21:17:19 -0300

    Fix Project

--- backend/src/main/java/com/projetosdev/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/projetosdev/dsmeta/services/SmsService.java
@@ -1,8 +1,5 @@
 package com.projetosdev.dsmeta.services;
 
-
-import java.text.DecimalFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
